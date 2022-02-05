e9aa83c53c997718733cbce67a953f16d52afae5
Beatriz Sobrino <beatriz.sobrino@cabify.com>
2022-02-05 20:47:48 +0100

    react router working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.scss';
 import { Menu } from './Menu';
+import { Routes, Route, Link } from "react-router-dom";
+import { Pieces } from './Pieces/PIeces';
+import { Rules } from './Rules/Rules';
+import { Game } from './Game/Game';
+
+export enum LinksTo {
+  pieces = 'PIECES',
+  game = 'GAME', 
+  rules = 'RULES'
+}
+
 
 function App() {
   return (
     <div className="App">
       <Menu/>
+      <Routes>
+        <Route path="/" />
+        <Route path={LinksTo.pieces} element={<Pieces />} />
+        <Route path={LinksTo.rules} element={<Rules />} />
+        <Route path={LinksTo.game} element={<Game />} />
+      </Routes>
     </div>
   );
 }

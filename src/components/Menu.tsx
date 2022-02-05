6963296913d92ad704b1bbcb1c072f4d62c2966b
Beatriz Sobrino <beatriz.sobrino@cabify.com>
2022-02-05 20:47:48 +0100

    react router working

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,13 @@
 import { MenuButton } from "./MenuButton"
 import './menu.scss'
+import { LinksTo } from "./App"
 
 export const Menu = () => {
   return (
     <nav className='menu'>
-      <MenuButton text="Reglas del Juego"/>
-      <MenuButton text="Fichas"/>
-      <MenuButton text="Juego"/>
+      <MenuButton text="Reglas del Juego" linkTo={LinksTo.rules}/>
+      <MenuButton text="Fichas" linkTo={LinksTo.pieces}/>
+      <MenuButton text="Juego" linkTo={LinksTo.game}/>
     </nav>
   )  
 }

371fa0792759ce6605bb87d1c13de081b3821dfb
Beatriz Sobrino <beatriz.sobrino@cabify.com>
2022-02-06 20:45:40 +0100

    finish game

--- src/components/Game/GamePage.tsx
+++ src/components/Game/GamePage.tsx
@@ -1,6 +1,9 @@
-import { useState } from 'react';
-import { DiceContainer } from './Dice/DiceContainer';
 import './gamePage.scss';
+import mando from '../../images/mando.png';
+import grogu from '../../images/groguGameOver.gif';
+import { useEffect, useState } from 'react';
+import { Modal } from '../Modal/Modal';
+import { DiceContainer } from './Dice/DiceContainer';
 import { Playground } from './Playground/Playground';
 
 export type DiceValueType = 'grogu' | 'egg' | 'frog' | 'cookie';
@@ -9,14 +12,33 @@ export const GamePage = () => {
     const [frogQuantity, setFrogQuantity] = useState<number>(3);
     const [eggQuantity, setEggQuantity] = useState<number>(3);
     const [cookieQuantity, setCookieQuantity] = useState<number>(3);
-    const [diceValue, setDiceValue ] = useState<DiceValueType>();
+    const [diceValue, setDiceValue ] = useState<DiceValueType|undefined>();
     const [groguPosition, setGroguPosition] = useState<number[]>([1,0,0,0,0,0,0,0]);
+    const [showWinModal, setShowWinModal] = useState<boolean>(false);
+    const [showGameOverModal, setShowGameOverModal] = useState<boolean>(true);
+
+    useEffect(() => {
+        if(!frogQuantity && !eggQuantity && !cookieQuantity){
+            setShowWinModal(true);
+        }  
+        if(groguPosition.indexOf(1) === 7) {
+            setShowGameOverModal(true)
+        }
+    },[frogQuantity,eggQuantity, cookieQuantity,groguPosition])
 
     const handleOnClick = () => {
         let dice = throwTheDice();
         updateState(dice);
     }
-
+    
+    const throwTheDice = () => {
+        const diceValues:DiceValueType[] = ['frog', 'egg','grogu', 'cookie'];
+        const randomNumber = Math.floor(Math.random() * 4)
+        const result = diceValues[randomNumber];
+        setDiceValue(result);
+        return result;
+    }
+    
     const updateState = (dice: DiceValueType) => {
         switch (dice) {
             case 'frog':
@@ -41,18 +63,22 @@ export const GamePage = () => {
         }
     }
 
-    const throwTheDice = () => {
-        const diceValues:DiceValueType[] = ['frog', 'egg','grogu', 'cookie'];
-        const randomNumber = Math.floor(Math.random() * 4)
-        const result = diceValues[randomNumber];
-        setDiceValue(result);
-        return result;
+    const handleModalOnClick = () => {
+        setCookieQuantity(3);
+        setEggQuantity(3);
+        setFrogQuantity(3);
+        setGroguPosition([1,0,0,0,0,0,0,0])
+        setDiceValue(undefined);
+        setShowGameOverModal(false);
+        setShowWinModal(false);
     }
 
     return (
         <main className='gamePage'>
             <DiceContainer diceValue={diceValue} handleOnClick={handleOnClick}/>
             <Playground groguPosition={groguPosition} frogQuantity={frogQuantity} eggQuantity={eggQuantity} cookieQuantity={cookieQuantity}/>
+            <Modal show={showWinModal} title="¡HAS GANADO!" image={mando} handleOnClick={handleModalOnClick}/>
+            <Modal show={showGameOverModal} title="¡OH NO! HAS PERDIDO ..." image={grogu} handleOnClick={handleModalOnClick}/>
         </main>
     )
 
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+import './modal.scss';
+
+interface ModalProps {
+    show: boolean;
+    title: string; 
+    handleOnClick: () => void;
+    image: string;
+}
+
+export const Modal = ({show, title, image, handleOnClick}: ModalProps) => {
+    if(!show) {
+        return null
+    }
+    return (
+        <div className="modal">
+            <div className='modal_content'>
+                <header className="modal_header">
+                    <h4 className="modal_header_title">
+                        {title}
+                    </h4>
+                </header>
+                <img className="modal_image" src={image} alt={title}/>
+                <footer className="modal_footer">
+                    <button onClick={handleOnClick} className="modal_footer_button">
+                        Volver a jugar
+                    </button>
+                </footer>
+            </div>
+         </div>
+
+
+    )
+}

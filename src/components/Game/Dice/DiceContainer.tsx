deaa90aaf112b8066afac6598790b5eb1117d967
Beatriz Sobrino <beatriz.sobrino@cabify.com>
2022-02-06 17:42:12 +0100

    add dice + logic dice

---
+++ src/components/Game/Dice/DiceContainer.tsx
@@ -0,0 +1,18 @@
+import './diceContainer.scss';
+import { DiceValueType } from "../GamePage";
+import { Dice } from "./Dice";
+import { DiceButton } from "./DiceButton";
+
+interface DiceContainerProps {
+    diceValue?: DiceValueType;
+    handleOnClick: () => void;
+}
+
+export const DiceContainer = ({diceValue,handleOnClick}: DiceContainerProps) =>{
+    return(
+        <div className='diceContainer'>
+                <Dice diceValue={diceValue}/>
+                <DiceButton handleOnClick={handleOnClick}/>
+        </div>
+    )
+}
